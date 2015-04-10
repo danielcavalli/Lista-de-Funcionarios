bffaac20f08bc8cc487e80c9d2dc302bc5b36547
Daniel Cavalli <deny_cavalli@hotmail.com>
2015-04-10 09:38:47 -0300

    Formulário de Funcionários
    Adiciona para a lista o nome do cadastrado

---
+++ ListaTrab/ListaTrab/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaTrab
+{
+    public partial class Form1 : Form
+    {
+        List<Funcionario> funcionarios = new List<Funcionario>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CarregarDados();
+            
+        }
+
+        private void CarregarDados()
+        {
+            for(int i = 1; i <= 130; i++)
+            {
+                comboBoxIdades.Items.Add(i);
+            }
+        }
+
+        private void Cadastrar(object sender, EventArgs e)
+        {
+            string nome = txtBoxNome.Text;
+            int idade = int.Parse(comboBoxIdades.Text);
+
+            Funcionario f = new Funcionario(idade, nome);
+
+            funcionarios.Add(f);
+            listBox1.Items.Add(f.nome);
+        }
+
+        private void comboBoxIdades_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Salvar_Click(object sender, EventArgs e)
+        {
+            f.asString();
+        }
+    }
+}
---
+++ ListaTrab/ListaTrab/Funcionario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaTrab
+{
+    class Funcionario
+    {
+        public string sexo, profissao, salario, estado_civil, filhos, tipos_sanguineos, nome, pais, estado, cidade, bairro, rua;
+        public int idade, telefone, numero;
+
+        public Funcionario(int idade, string nome/*, int numero, int telefone, string sexo, string profissao, string salario, string estado_civil, string filhos, 
+               string tipos_sanguineos, string pais, string estado, string cidade, string bairro, string rua*/)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            /*this.profissao = profissao;
+            this.salario = salario;
+            this.estado_civil = estado_civil;
+            this.filhos = filhos;
+            this.tipos_sanguineos = tipos_sanguineos;
+            this.pais = pais;
+            this.estado = estado;
+            this.cidade = cidade;
+            this.bairro = bairro;
+            this.rua = rua;
+            this.idade = idade;
+            this.sexo = sexo;
+            this.telefone = telefone;
+            this.numero = numero;*/
+        }
+        public string asString()
+        {
+            return nome + " " + idade;
+        }
+    }
+}
